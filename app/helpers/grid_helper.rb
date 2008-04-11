4efda56dc75bce31745c9ded9f457e4210bf7ec9
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-11 13:05:10 +0000

    add "first" class to first child
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@820 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/grid_helper.rb
+++ app/helpers/grid_helper.rb
@@ -1,10 +1,17 @@
 module GridHelper
-  def render_grid(grid)
+  # Renders the given grid and all its containing grids (and later parts)
+  # warning: recursive
+  def render_grid(grid,opts = {})
     if grid.children.empty?
       inner = h(grid.to_s)
+      css_class = grid.grid_class
+      css_class += ' first' if opts[:is_first]
     else
-      inner = grid.children.collect {|child| render_grid(child)}.join(' ')
+      inner = grid.children.collect do |child| 
+        render_grid(child, :is_first => (child == grid.children.first))
+      end.join(' ')
+      css_class = grid.grid_class
     end
-    content_tag(:div, inner, {:class => grid.grid_class})
+    content_tag(:div, inner, {:class => css_class})
   end
 end
