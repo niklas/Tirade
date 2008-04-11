638e4d20a2708a9688dc8ef3581d8e32996b9d77
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-11 17:41:47 +0000

    _() stub -- please prepare for translation
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@834 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+
+  # just a placeholder for translations (localisation plugin)
+  def _(phrase)
+    phrase
+  end
 end
