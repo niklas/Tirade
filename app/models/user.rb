2c1b9035d3f05e199b4ec3492f2d85b2b751d89d
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-11 15:45:31 +0000

    annotated model(s)
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@832 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+# Schema version: 2
+#
+# Table name: users
+#
+#  id            :integer         not null, primary key
+#  name          :string(255)     
+#  password_hash :string(40)      
+#  password_salt :string(40)      
+#  verified_at   :datetime        
+#  email         :string(255)     
+#  remember_me   :string(40)      
+#  created_at    :datetime        
+#  updated_at    :datetime        
+#
+
 class User < ActiveRecord::Base
   acts_as_authenticated_user
+
+  def username_field
+    'name'
+  end
 end
