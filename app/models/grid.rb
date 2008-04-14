78d65299a46e8884c0b0b40babee049a76eb2954
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-14 16:25:13 +0000

    grid should change its grid_class after adding children
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@853 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -23,20 +23,48 @@ class Grid < ActiveRecord::Base
     'yui-gd' =>	'1/3 - 2/3',
     'yui-ge' =>	'3/4 - 1/4',
     'yui-gf' =>	'1/4 - 3/4',
-    'yui-u'  =>   'single'
+    'single'  =>  '100%',
+    'yui-u'  =>   'empty'
+  }
+  IdealChildrenCount = {
+    'yui-g'  =>   2,
+    'yui-gb' =>	3,
+    'yui-gc' =>	2,
+    'yui-gd' =>	2,
+    'yui-ge' =>	2,
+    'yui-gf' =>	2,
+    'single' =>	1,
+    'yui-u'  =>   0
   }
   validates_inclusion_of :grid_class, :in => Types
+
+  def after_destroy
+    parent.andand.save
+  end
+
+  def before_save
+    auto_yui_class!
+  end
 
   def self.new_by_grid_class(grid_class)
     new(:grid_class => grid_class)
   end
 
   def add_child(child_grid=nil)
-    self.save if new_record?
+    self.save! if new_record?
     child_grid ||= self.class.new_by_grid_class('yui-u')
     child_grid.save!
     child_grid.move_to_child_of self
+    self.save!
     child_grid
+  end
+
+  def visible_children
+    children.first(ideal_children_count)
+  end
+
+  def ideal_children_count
+    IdealChildrenCount[self.grid_class] || 0
   end
 
   # A wrapper to return the proper YUI class depending on +self+'s position
@@ -55,5 +83,17 @@ class Grid < ActiveRecord::Base
 
   def is_first_child?
     self == self.parent.andand.children.andand.first
+  end
+
+  def auto_yui_class!
+    if ideal_children_count != children.size && new_class = self.auto_yui_class
+      self.grid_class = new_class
+    end
+  end
+
+  protected
+  def auto_yui_class
+    actual_count = self.children.length
+    IdealChildrenCount.sort.find {|name,count| count == actual_count}.andand.first
   end
 end
--- spec/models/grid_spec.rb
+++ spec/models/grid_spec.rb
@@ -5,7 +5,7 @@ describe Grid do
     @grid = Grid.new
   end
 
-  it "should_not be valid" do
+  it "should not be valid" do
     @grid.should_not be_valid
   end
 
@@ -13,6 +13,55 @@ describe Grid do
     @grid.grid_class = Grid::Types.keys.first
     @grid.should be_valid
   end
+
+  describe 'after save' do
+    before(:each) do
+      @grid.grid_class = Grid::Types.keys.first
+      @grid.save!
+    end
+    it "should have lft and rgt attributes" do
+      @grid.lft.should_not be_nil
+      @grid.rgt.should_not be_nil
+    end
+
+    describe ', adding a child' do
+      before(:each) do
+        @grid.add_child
+      end
+      it "should change its gridclass to 'single'" do
+        @grid.grid_class.should == 'single'
+      end
+
+      it "should change its ideal number of children to 1" do
+        @grid.ideal_children_count.should == 1
+      end
+
+      describe ', adding a second child' do
+        before(:each) do
+          @grid.add_child
+        end
+        it "should change its gridclass to 50-50" do
+          @grid.grid_class.should == 'yui-g'
+        end
+        it "should change its ideal number of children to 2" do
+          @grid.ideal_children_count.should == 2
+        end
+
+        describe ', adding a third child' do
+          before(:each) do
+            @grid.add_child
+          end
+          it "should change its gridclass to 33-33-33" do
+            @grid.grid_class.should == 'yui-gb'
+          end
+          it "should change its ideal number of children to 3" do
+            @grid.ideal_children_count.should == 3
+          end
+        end
+      end
+    end
+  end
+
 
 end
 
