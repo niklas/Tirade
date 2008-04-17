ecd7dd024a51a6412aff99a44969709e5eaed977
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-17 13:13:45 +0000

    annotated models
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@893 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+# Schema version: 5
+#
+# Table name: contents
+#
+#  id           :integer         not null, primary key
+#  title        :string(255)     
+#  description  :text            
+#  body         :text            
+#  type         :string(255)     
+#  state        :string(255)     
+#  owner_id     :integer         
+#  published_at :datetime        
+#  position     :integer         
+#  parent_id    :integer         
+#  lft          :integer         
+#  rgt          :integer         
+#  created_at   :datetime        
+#  updated_at   :datetime        
+#
+
 class Content < ActiveRecord::Base
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 4
+# Schema version: 5
 #
 # Table name: users
 #
