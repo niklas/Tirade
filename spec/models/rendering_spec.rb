b1f50079e07cb2e84c8305f22cd71b8e1cb29c16
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-23 13:58:51 +0000

    spec for assigning an Image to a recnering as #content
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@965 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- spec/models/rendering_spec.rb
+++ spec/models/rendering_spec.rb
@@ -35,9 +35,6 @@ describe Rendering do
     Rendering.reflections[:content].should_not be_nil
     @rendering.should respond_to(:content)
   end
-
-  it "the content association should be polymorph"
-  it "the content association should access other tables than 'content'"
 end
 
 describe Rendering, ' appended to the left column of the main page, containing a standard Content' do
@@ -89,6 +86,36 @@ describe Rendering, ' appended to the left column of the main page, containing a
       found_content = @page.renderings.for_grid(@grid).last.content
       found_content.should be_instance_of(Document)
       found_content.should == @goodbye
+    end.should_not raise_error
+  end
+end
+
+describe Rendering, ' appended to the left column of the main page, containing an Image' do
+  fixtures :all
+  before(:each) do
+    @page = pages(:main)
+    @grid = grids(:layout_50_50_1)
+    @content = images(:landscape)
+    lambda do
+      @rendering = Rendering.create!(
+        :page => @page,
+        :grid => @grid,
+        :content => @content,
+        :part => parts(:simple_preview)
+      )
+    end.should change(Rendering,:count).by(1)
+  end
+  it "should be the last item in the left column" do
+    @page.renderings.for_grid(@grid).last.should == @rendering
+  end
+  it "should have the correct content_type set" do
+    @rendering.content_type.should == 'Image'
+  end
+  it "should find the content" do
+    lambda do
+      found_content = @page.renderings.for_grid(@grid).last.content
+      found_content.should be_instance_of(Image)
+      found_content.should == @content
     end.should_not raise_error
   end
 end
