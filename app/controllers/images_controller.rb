2585f5574b4381b618ecd0b624febe7f3b38a60f
alex <alex@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-29 22:27:39 +0000

    Basic upload functionality and title generation. Removed paperclip as external, because it was outdated. I will clone the new version on the next commit.
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1040 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,6 +3,28 @@ class ImagesController < ApplicationController
   
   def index
     # TODO: Pagination
+    @images = Image.find(:all, :order => 'created_at DESC')
     
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    @image = Image.new(params[:image])
+    
+    if @image.save
+      redirect_to images_path
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    
+    redirect_to(images_path)
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -15,14 +15,24 @@
 # FIXME: Temporary location for the image styles configuration
 CONFIG = {}
 CONFIG[:thumbnail_size] = '60x60#'
-CONFIG[:upload_images_path] = '/public/upload/'
+CONFIG[:upload_images_path] = '/upload/'
 
 class Image < ActiveRecord::Base
   has_attached_file :image,
                     :styles => {:thumbnail => CONFIG[:thumbnail_size]},
                     :default_style => :thumbnail,
-                    :path => ":rails_root#{CONFIG[:upload_images_path]}:attachment/:id/:style/:basename.:extension"
-
+                    :path => ":rails_root/public#{CONFIG[:upload_images_path]}:attachment/:id/:style/:basename.:extension",
+                    :url => "#{CONFIG[:upload_images_path]}/:attachment/:id/:style/:basename.:extension"
   
   has_fulltext_search :title
+  
+  before_create :generate_title
+  
+  private
+  def generate_title
+    # Remove last extension
+    title = self.image_file_name.split('.')
+    title = title[0..-2].join
+    self.title = title.humanize.titlecase
+  end
 end
