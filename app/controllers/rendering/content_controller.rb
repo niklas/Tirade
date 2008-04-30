6a4cdc5b3f47c23bdee5e0f08d61a2598d003283
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-04-30 15:57:19 +0000

    show nothing if no content was found
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1081 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/controllers/rendering/content_controller.rb
+++ app/controllers/rendering/content_controller.rb
@@ -5,9 +5,13 @@ class Rendering::ContentController < ApplicationController
   # Search for all valid content types
   def new
     @term = params[:term]
-    @contents = Rendering.valid_content_types.inject([]) do |found,klass|
-      found + klass.search(@term)
-    end.flatten
+    @contents = unless @term.blank?
+                  Rendering.valid_content_types.inject([]) do |found,klass|
+                    found + klass.search(@term)
+                  end.flatten.uniq
+                else
+                  []
+                end
     respond_to do |wants|
       wants.js
     end
