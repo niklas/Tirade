fced5173e96e08008c891a6791cf67f924008dda
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-02 13:57:13 +0000

    contents list spec
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1089 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- spec/views/contents/index.html.erb_spec.rb
+++ spec/views/contents/index.html.erb_spec.rb
@@ -5,43 +5,33 @@ describe "/contents/index.html.erb" do
   
   before(:each) do
     content_98 = mock_model(Content)
-    content_98.should_receive(:title).and_return("MyString")
-    content_98.should_receive(:description).and_return("MyText")
-    content_98.should_receive(:body).and_return("MyText")
-    content_98.should_receive(:type).and_return("MyString")
-    content_98.should_receive(:state).and_return("MyString")
-    content_98.should_receive(:owner_id).and_return("1")
-    content_98.should_receive(:published_at).and_return(Time.now)
-    content_98.should_receive(:position).and_return("1")
-    content_98.should_receive(:parent_id).and_return("1")
-    content_98.should_receive(:lft).and_return("1")
-    content_98.should_receive(:rgt).and_return("1")
+    content_98.should_receive(:title).and_return("Title 98")
+    content_98.should_receive(:description).and_return("Description 98")
+    content_98.should_receive(:body).and_return("Body 98")
+    content_98.should_receive(:updated_at).and_return(Time.now - 10.seconds)
+    
     content_99 = mock_model(Content)
-    content_99.should_receive(:title).and_return("MyString")
-    content_99.should_receive(:description).and_return("MyText")
-    content_99.should_receive(:body).and_return("MyText")
-    content_99.should_receive(:type).and_return("MyString")
-    content_99.should_receive(:state).and_return("MyString")
-    content_99.should_receive(:owner_id).and_return("1")
-    content_99.should_receive(:published_at).and_return(Time.now)
-    content_99.should_receive(:position).and_return("1")
-    content_99.should_receive(:parent_id).and_return("1")
-    content_99.should_receive(:lft).and_return("1")
-    content_99.should_receive(:rgt).and_return("1")
-
+    content_99.should_receive(:title).and_return("Title 99")
+    content_99.should_receive(:description).and_return("Description 99")
+    content_99.should_receive(:body).and_return("Body 99")
+    content_99.should_receive(:updated_at).and_return(Time.now.yesterday)
     assigns[:contents] = [content_98, content_99]
   end
 
   it "should render list of contents" do
     render "/contents/index.html.erb"
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyText", 2)
-    response.should have_tag("tr>td", "MyText", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "MyString", 2)
-    response.should have_tag("tr>td", "1", 2)
-    response.should have_tag("tr>td", "1", 2)
-    response.should have_tag("tr>td", "1", 2)
+    response.should have_tag("tr>td>a", "Title 98", 1)
+    response.should have_tag("tr>td", "Description 98", 1)
+    response.should have_tag("tr>td", "Body 98", 1)
+    response.should have_tag("tr>td", /less than a minute ago/)
+    response.should have_tag("tr>td>a", "show", 2)
+    response.should have_tag("tr>td>a", "edit", 2)
+    response.should have_tag("tr>td>a", "Destroy", 2)
+
+    response.should have_tag("tr>td>a", "Title 99", 1)
+    response.should have_tag("tr>td", "Description 99", 1)
+    response.should have_tag("tr>td", "Body 99", 1)
+    response.should have_tag("tr>td", /1 day ago/)
   end
 end
 
