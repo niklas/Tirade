b5442ebb4dc84d83844d006733a5820f43966578
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-06 15:40:29 +0000

    patched ThemeSupport to support Sass. or vice versa.
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1122 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- lib/monkey_patches.rb
+++ lib/monkey_patches.rb
@@ -28,3 +28,27 @@ class String
     strip.gsub(/[^\w]+/,'-').sub(/-\Z/,'').downcase
   end
 end
+
+
+module Sass
+  module ThemeSupport
+    # Patch the process to set SASS path to the current theme, thus supporting sass stylesheets in themes
+    def process(*args)
+      if defined?(Sass)
+        theme = args.first.parameters[:theme]
+        old_sass_options = Sass::Plugin.options
+        Sass::Plugin.options.merge!(:template_location  => RAILS_ROOT + "/themes/#{theme}/stylesheets/sass",
+                                    :css_location       => RAILS_ROOT + "/themes/#{theme}/stylesheets",
+                                    :always_check       => RAILS_ENV != "production",
+                                    :always_update      => RAILS_ENV != "production",
+                                    :full_exception     => RAILS_ENV != "production")
+        # FIXME do not render sass stylesheets on every request
+        Sass::Plugin.update_stylesheets
+        Sass::Plugin.options.merge!(old_sass_options)
+      end
+      super
+    end
+  end
+end
+
+ThemeController.send :include, Sass::ThemeSupport
