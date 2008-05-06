d1015860290194318b58588d2b367b7618b59d5b
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-06 22:08:17 +0000

    flashobject installed
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1125 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,4 +37,15 @@ module ApplicationHelper
     link_to(label,public_content_path(content.url))
   end
 
+  # copied without shame from http://blog.vixiom.com/2007/06/05/rails-helper-for-swfobject/
+  def swf_object(swf, id, width, height, flash_version, background_color, params = {}, vars = {}, create_div = false)
+    # create div ?
+    create_div ? output = "<div id=’#{id}‘>This website requires <a href=’http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash&promoid=BIOW’ target=’_blank’>Flash player</a> #{flash_version} or higher.</div><script type=’text/javascript’>" : output = "<script type=’text/javascript’>"
+    output << "var so = new SWFObject(’#{swf}‘, ‘#{id}‘, ‘#{width}‘, ‘#{height}‘, ‘#{flash_version}‘, ‘#{background_color}‘);"
+    params.each  {|key, value| output << "so.addParam(’#{key}‘, ‘#{value}‘);"}
+    vars.each    {|key, value| output << "so.addVariable(’#{key}‘, ‘#{value}‘);"}
+    output << "so.write(’#{id}‘);"
+    output << "</script>"
+  end
+
 end
