558d3d22a5e18fcde3ec112cb55c355aea8279a6
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-08 18:01:39 +0000

    no scaled image if there is now image
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1178 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -6,6 +6,7 @@ module ImagesHelper
   end
 
   def scaled_image_tag(record_with_image,geom,opts={})
+    return '' unless record_with_image
     image = record_with_image.is_a?(Image) ? record_with_image : record_with_image.image
     image_tag(image.custom_thumbnail_url(geom),opts)
   end
