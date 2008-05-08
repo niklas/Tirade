57137f99dfb8aa6d4144a7f1e7ae55c4abfc1690
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-08 11:17:49 +0000

    do not remotify link_to_remote links
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1153 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- public/javascripts/behaviours.js
+++ public/javascripts/behaviours.js
@@ -8,7 +8,7 @@ Event.addBehavior({
     'div.grid form.destroy.grid': Remote,
     'body.role_admin div.rendering' : ContentEditable,
     'div#toolbox form': Remote,
-    'div#toolbox a': Remote,
+    'div#toolbox a[href!="#"]': Remote,
     'div#toolbox form.update.rendering': Observed(previewRendering,{frequency: 5}),
     'div#toolbox form.update.content': Observed(previewRendering,{frequency: 5}),
     'div#toolbox form.update.part': Observed(previewRendering,{frequency: 5}),
