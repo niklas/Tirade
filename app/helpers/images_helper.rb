a2a9958cb8c5ca9cb7ab970f5541d017186185bd
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-08 23:22:11 +0000

    alt+title for scaled_images
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1189 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -8,6 +8,8 @@ module ImagesHelper
   def scaled_image_tag(record_with_image,geom,opts={})
     return '' unless record_with_image
     image = record_with_image.is_a?(Image) ? record_with_image : record_with_image.image
+    opts[:title] ||= image.title
+    opts[:alt] ||= image.title
     image_tag(image.custom_thumbnail_url(geom),opts)
   end
 end
