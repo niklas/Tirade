3a9e6789ab3c59062fb24ca0f6d6fb6ee6f21975
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-18 23:59:25 +0000

    fixed some stubs for paagespec
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1240 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- spec/views/pages/show.html.erb_spec.rb
+++ spec/views/pages/show.html.erb_spec.rb
@@ -11,6 +11,9 @@ describe "/pages/show.html.erb" do
     @page.stub!(:parent).and_return(nil)
     @page.stub!(:final_layout).and_return(nil)
     @page.stub!(:yui_name).and_return('111%')
+    @page.stub!(:root?).and_return(true)
+    @page.stub!(:children).and_return([])
+    @page.stub!(:renderings).and_return([])
 
     assigns[:page] = @page
   end
@@ -22,5 +25,7 @@ describe "/pages/show.html.erb" do
     response.should have_text(/1/)
     response.should have_text(/1/)
   end
+
+  it "should render a page hierarchy with itself highlighted"
 end
 
