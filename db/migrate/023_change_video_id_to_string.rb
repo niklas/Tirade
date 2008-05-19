0e2828f9dd2a29dcfc5cfbef5a0a8abeb52cb0de
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-05-19 21:57:10 +0000

    no strings for videos. foreign key of rendering would be a problem.. please drop all your videos
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1256 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- db/migrate/023_change_video_id_to_string.rb
+++
@@ -1,9 +0,0 @@
-class ChangeVideoIdToString < ActiveRecord::Migration
-  def self.up
-    change_column :videos, :id, :string
-  end
-
-  def self.down
-    change_column :videos, :id, :integer
-  end
-end
