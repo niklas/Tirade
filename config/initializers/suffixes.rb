5304808c73cd8d10045908be784ea1a799075edf
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-07-30 17:28:00 +0000

    use resourceful views plugin where we can
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1281 9df96bbd-a318-0410-8f51-977bef1cc4bb

---
+++ config/initializers/suffixes.rb
@@ -0,0 +1,6 @@
+# For the ResourcefulViews plugin
+# This will append '_link' to all link helpers, like 'edit_table_top_link'
+ResourcefulViews.link_helpers_suffix = '_link'
+
+# This will append '_form' to all link helpers, like 'search_tables_form'
+ResourcefulViews.form_helpers_suffix = '_form'
