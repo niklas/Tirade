cf448ad020b98432dffdd0039044d333d5564795
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-08-02 03:37:37 +0000

    nice colored links in a bar.
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1301 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/interface_helper.rb
+++ app/helpers/interface_helper.rb
@@ -8,4 +8,16 @@ module InterfaceHelper
     concat content_tag(:h3,title, :class => 'accordion_toggle'), block.binding
     concat content_tag(:div,capture(&block), :class => 'accordion_content'), block.binding
   end
+
+  def links(&block)
+    concat content_tag(:ul,capture(&block), :class => 'linkbar'), block.binding
+  end
+
+  def li_link_to(name, options = {}, html_options = nil)
+    content_tag(:li,link_to(name,options,html_options))
+  end
+
+  def li_link_to_remote(name, options = {}, html_options = nil)
+    content_tag(:li,link_to_remote(name,options,html_options))
+  end
 end
--- lib/tirade/normal_form_builder.rb
+++ lib/tirade/normal_form_builder.rb
@@ -48,17 +48,16 @@ class NormalFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def submit(label="Submit", opts={})
-    returning '' do |html|
-      if @object.new_record?
-        html << super('Create', :class => 'submit create')
-        html << @template.link_to('cancel', {:controller => @object_name.pluralize}) 
-      else
-        html << super('Save', :class => 'submit save')
-        html << @template.link_to('cancel', @object) 
-        html << " | "
-        html << @template.link_to('back', {:controller => @object_name.pluralize}) 
-      end
+    html = []
+    if @object.new_record?
+      html << super('Create', :class => 'submit create')
+      html << @template.link_to('cancel', {:controller => @object_name.pluralize}) 
+    else
+      html << super('Save', :class => 'submit save')
+      html << @template.link_to('cancel', @object) 
+      html << @template.link_to('back', {:controller => @object_name.pluralize}) 
     end
+    html.map {|b| @template.content_tag(:li,b)}.join(' ')
   end
 
   private
