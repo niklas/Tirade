e586df42aafaa0c8e3548d5a95f174dce91a0521
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-08-03 15:00:14 +0000

    created toolboxhelper
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1318 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/renderings_helper.rb
+++ app/helpers/renderings_helper.rb
@@ -1,30 +1,5 @@
 module RenderingsHelper
 
-  def update_toolbox(opts = {})
-    if header = (opts[:header] || opts[:top])
-      page[:toolbox_top].replace_html header
-    end
-
-    page[:toolbox_content].replace_html opts[:content] if opts[:content]
-    
-    if footer = (opts[:footer] || opts[:bottom] || page.context.flash[:notice])
-      page[:toolbox_bottom].replace_html footer
-    end
-    if nav_name = opts[:nav]
-      page[:toolbox_sidebar].replace_html tabnav_to_s(nav_name)
-    else
-      page[:toolbox_sidebar].replace_html tabnav_to_s('main')
-    end
-  end
-
-  def tabnav_to_s(name)
-    partial_name = "widgets/#{name}_tabnav"
-    render :partial => partial_name
-  end
-
-  def close_toolbox
-    page.select('#toolbox').each {|tb| tb.remove }
-  end
   def update_rendering(rendering)
     dom = page.context.dom_id(rendering)
     page[dom].replace rendering.render
