3055b96e95c81ea9c18d9cca907ea435911c6a9c
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-08-05 14:14:17 +0000

    do not break if application.yml is missing
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1346 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,5 @@
-APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/application.yml")[RAILS_ENV]
+begin
+  APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/application.yml")[RAILS_ENV]
+rescue
+  puts "could not find config/application.yml - some features/setting may be missing."
+end
