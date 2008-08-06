67fc9a9bfb8f723979c9e820dec90b8b0091a21f
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-08-06 18:10:31 +0000

    do not re-open User in stylish permissions (should solve the noMethodError)
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1362 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- vendor/plugins/stylish_permissions/app/models/user.rb
+++ vendor/plugins/stylish_permissions/app/models/user.rb
@@ -1,30 +1,32 @@
-class User < ActiveRecord::Base
-  has_many :roles, :through => :user_roles, :dependent => :destroy
-  has_many :user_roles
-  has_and_belongs_to_many :groups
+module StylishPermissions
+  module User
+    def self.included(base)
+      base.has_many :roles, :through => :user_roles, :dependent => :destroy
+      base.has_many :user_roles
+      base.has_and_belongs_to_many :groups
+    end
+    # FIXME use first_name and last_name
+    def full_name
+      login
+    end
 
+    def permissions
+      (
+        roles.map {|r| r.permissions } +
+        groups.map {|g| g.permissions }
+      ).flatten.uniq
+    end
 
-  # FIXME use first_name and last_name
-  def full_name
-    login
-  end
+    def permissions_names
+      permissions.map { |perm| perm.name }
+    end
 
-  def permissions
-    (
-      roles.map {|r| r.permissions } +
-      groups.map {|g| g.permissions }
-    ).flatten.uniq
-  end
+    def roles_names
+      roles.all.map {|r| r.name }
+    end
 
-  def permissions_names
-    permissions.map { |perm| perm.name }
-  end
-
-  def roles_names
-    roles.all.map {|r| r.name }
-  end
-
-  def roles_short_names
-    roles.all.map {|r| r.short_name }
+    def roles_short_names
+      roles.all.map {|r| r.short_name }
+    end
   end
 end
--- vendor/plugins/stylish_permissions/init.rb
+++ vendor/plugins/stylish_permissions/init.rb
@@ -1,2 +1,4 @@
 require_plugin 'restful_authentication'
 require_plugin 'toolbox'
+
+User.class_eval { include StylishPermissions::User }
