53341a42f645601094697c5d65c447bab81a96ad
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-08-08 14:14:17 +0000

    some more toolbox steps
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1398 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- stories/steps/toolbox_steps.rb
+++ stories/steps/toolbox_steps.rb
@@ -1,12 +1,41 @@
 steps_for(:toolbox) do
-  Then 'the Toolbox should open' do
+  Then /the Toolbox (should|should not) (?:be |stay )?open(?: anymore)?/ do |yes_or_no|
     wait_for_ajax
-    $browser.is_element_present("css=div#toolbox")
-    $browser.is_visible("css=div#toolbox")
+    if yes_or_no == 'should'
+      $browser.is_element_present("css=div#toolbox").should be_true
+      $browser.is_visible("css=div#toolbox").should be_true
+    else
+      (
+        !$browser.is_element_present("css=div#toolbox") ||
+        !$browser.is_visible("css=div#toolbox")
+      ).should be_true
+    end
   end
 
   When 'submitting toolbox form $form' do |form|
     $browser.submit form
     wait_for_ajax
+  end
+
+  When /(he|she) closes the Toolbox/ do |_|
+    $browser.click "css=div#toolbox_close"
+    wait_for_effect
+  end
+
+  When /(she|he) opens accordion(?: item )? (\S+)/ do |_,item|
+    $browser.click "css=h3.accordion_toggle.#{item}"
+    wait_for_effect
+  end
+
+  When /(he|she) clicks on toolbox link to (\w+) (\w+)/ do |_,action,controller|
+    $browser.click "css=#toolbox a.#{action}.#{controller}"
+    wait_for_ajax
+    wait_for_effect
+  end
+
+  When /(he|she) clicks on toolbox button named (\w+)/ do |_,name|
+    $browser.click %Q~css=#toolbox input[value="#{name}"]~
+    wait_for_ajax
+    wait_for_effect
   end
 end
