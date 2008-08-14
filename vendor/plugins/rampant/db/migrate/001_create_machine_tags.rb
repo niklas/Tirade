39b70a003aec1e7d46321071db7992cb864bdacb
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-08-14 15:02:04 +0000

    normalized machine tags (namespace,key,value) as told in http://geobloggers.com/2006/01/11/advanced-tagging-and-tripletags/
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1420 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- vendor/plugins/rampant/db/migrate/001_create_machine_tags.rb
+++ vendor/plugins/rampant/db/migrate/001_create_machine_tags.rb
@@ -1,14 +1,16 @@
 class CreateMachineTags < ActiveRecord::Migration
   def self.up
     create_table :machinetags do |t|
-      t.string :name, :limit => 128
-      # TODO split names into namespace:"predicate"
-      # t.string :predicate, :limit => 1024
+      t.string :namespace, :limit => 128
+      t.string :key, :limit => 128
+      t.string :value, :limit => 1024
       t.integer :machinetaggings_count, :default => 0, :null => false
 
       t.timestamps
     end
-    add_index :machinetags, :name, :name => "index_mtags_on_name"
+    add_index :machinetags, :namespace, :name => "index_mtags_on_namespace"
+    add_index :machinetags, :key, :name => "index_mtags_on_key"
+    add_index :machinetags, :value, :name => "index_mtags_on_value"
     add_index :machinetags, :machinetaggings_count, :name => "index_mtags_on_mtaggings_count"
   end
 
