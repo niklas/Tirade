a4d95ab8974e428a73e3717fcdedb45ca45ddd7e
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-10-13 15:13:45 +0000

    jquery.resourceID fixed
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1454 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -5,9 +5,13 @@
 jQuery.fn.resourceId = function() {
   if (m = $(this).attr('id').match(/(\d+)$/)) {
     return(m[1]);
-  } else if (m = $(this).children('a.rendering')[0].attr['href'].match(/(\d+)$/)) {
+  } else if (
+    (first_href = $(this).children('a:first[@href!=#]').attr('href')) &&
+    (m = first_href.match(/(\d+)$/))) {
     return(m[1]);
-  } else if (m = $(this).attr['action'].match(/(\d+)\D*$/)) {
+  } else if (
+    (action = $(this).attr('action')) && 
+    (action.match(/(\d+)\D*$/))) {
     return(m[1]);
   }
 }
@@ -18,14 +22,14 @@ $(function() {
   $('a.with_toolbox').livequery(function() { $(this).useToolbox(); });
 
   $('div#toolbox > div.body > div.content > div.frame a[href!=#]:not([class=back])').livequery(function() { $(this).useToolbox(); })
-  $('body div.grid').each(function(i) {
+  $('body div.grid').livequery(function() {
     $(this).appendDom([
       { tagName: 'div', class: 'admin', id: 'admin_' + $(this).attr('id'), childNodes: [
         { tagName: 'a', href: edit_grid_url({id: $(this).resourceId()}), class: 'edit grid', innerHTML: 'edit' }
       ] }
     ])}
   );
-  $('body div.rendering').each(function(i) {
+  $('body div.rendering').livequery(function(i) {
     $(this).appendDom([
       { tagName: 'div', class: 'admin', id: 'admin_' + $(this).attr('id'), childNodes: [
         { tagName: 'a', href: rendering_url({id: $(this).resourceId()}), class: 'edit rendering', innerHTML: 'edit' },
