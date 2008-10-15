c2b91f0384fb95eb936f16c6de85e981338c1d3c
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-10-15 19:48:36 +0000

    render_attributes_for content
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1462 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -21,4 +21,23 @@ module ContentsHelper
       raise "No fields partial found for #{content.class}" 
     end
   end
+
+  def render_attributes_for(content)
+    klass = content.class
+    begin
+      partial_name = "/contents/#{klass.to_s.underscore}_attributes"
+      logger.debug("render_attributes_for: trying partial '#{partial_name}'")
+      render(:partial => partial_name, :object => content)
+    rescue ActionView::ActionViewError => e
+      @errors ||= []
+      @errors << e
+      logger.debug("render_form_fields_for: failed: #{e.message}")
+      unless Content == klass
+        klass = klass.superclass
+        retry
+      else
+        render(:partial => '/contents/attributes', :object => content)
+      end
+    end
+  end
 end
