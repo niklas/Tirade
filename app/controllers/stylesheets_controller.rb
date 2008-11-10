a226cad4fa7f121463dfdb4f753be4528b50bccd
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-11-10 19:24:04 +0000

    Stylesheets: define colors in controller, use them in toolbox_colors
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1521 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -1,5 +1,6 @@
 class StylesheetsController < ApplicationController
   before_filter :set_headers
+  before_filter :set_vars
   after_filter { |controller| controller.cache_page }
   session :off
   layout nil
@@ -8,5 +9,30 @@ class StylesheetsController < ApplicationController
 
   def set_headers
     headers['Content-Type'] = 'text/css; charset=utf-8'
+  end
+
+  def set_vars
+    # Margins / Paddings
+    @small  = '0.3em'
+    @medium = '0.7em'
+    @big    = '1.3em'
+    @colors = {
+      :base         => '#f0f0f0',
+
+      :hover        => '#3198F2',
+      :perimeter    => '#dedede',
+
+      :toolbox_border => '#cdcdcd',
+      :toolbox_border_dark => '#898989',
+      :decoration     => '#474747',
+      :warning      => '#c72c1e',
+      :selected     => '#bdbdbd',
+      :text         => '#222222',
+
+      :element      => '#bbbbbb',
+      :border_dark  => '#dedede',
+      :clickable    => '#133B5E',
+    }
+    @perimeter = "1px solid #{@colors[:perimeter]}" # border around elements
   end
 end
