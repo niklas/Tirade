8d5c447c956263f9fdafe11d472a136c4dd91e7f
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-11-19 18:34:36 +0000

    check pages/edit only for form and fields
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1580 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- spec/views/pages/edit.html.erb_spec.rb
+++ spec/views/pages/edit.html.erb_spec.rb
@@ -18,26 +18,18 @@ describe "/pages/edit.html.erb" do
     template.controller.stub!(:controller_name).and_return("pages")
   end
 
-  it "should render edit form" do
+  it "should render edit form with some fields" do
     render "edit.html.erb"
     
     response.should have_tag("form[action=#{page_path(@page)}][method=post]") do
-      with_tag('div.title') do
-        with_tag('label', 'Title')
-        with_tag('input#page_title[name=?]', "page[title]")
-      end
-      with_tag('div.parent_page') do
-        with_tag('label', 'Parent Page')
-        with_tag('select#page_wanted_parent_id[name=?]', "page[wanted_parent_id]")
-      end
-      with_tag('div.layout') do
-        with_tag('label', 'Layout')
-        with_tag('select#page_layout_id[name=?]', "page[layout_id]")
-      end
-      with_tag('div.width') do
-        with_tag('label', 'Width')
-        with_tag('select#page_yui[name=?]', "page[yui]")
-      end
+    #    with_tag('label', 'Title')
+       with_tag('input#page_title[name=?]', "page[title]")
+    #    with_tag('label', 'Parent Page')
+       with_tag('select#page_wanted_parent_id[name=?]', "page[wanted_parent_id]")
+    #    with_tag('label', 'Layout')
+       with_tag('select#page_layout_id[name=?]', "page[layout_id]")
+    #    with_tag('label', 'Width')
+       with_tag('select#page_yui[name=?]', "page[yui]")
     end
   end
 end
