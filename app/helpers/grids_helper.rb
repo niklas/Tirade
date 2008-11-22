5789ccb1fbe56986d164b0a8aae93f2e1b685de7
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-11-22 19:30:06 +0000

    mark active grid as active.. must set @grid for that...
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1604 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/helpers/grids_helper.rb
+++ app/helpers/grids_helper.rb
@@ -1,7 +1,7 @@
 module GridsHelper
   def show_grid(grid)
     render_grid(grid, :class => 'preview') do |g|
-      content_tag(:span, g.label, :class => 'label')
+      content_tag(:span, g.label, :class => "label #{'active' if g==@grid }")
     end
   end
 end
