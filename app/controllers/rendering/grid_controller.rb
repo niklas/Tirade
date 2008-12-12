052ddddfbc9961203e65c819a1f0909e8a467575
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-12-12 20:32:26 +0000

    removed nested controller /manage/rendering
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1684 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/controllers/rendering/grid_controller.rb
+++
@@ -1,9 +0,0 @@
-class Rendering::GridController < ApplicationController
-  before_filter :fetch_rendering
-  def edit
-    @grid = @rendering.grid
-    respond_to do |wants|
-      wants.js
-    end
-  end
-end
--- app/helpers/rendering/content_helper.rb
+++
@@ -1,2 +0,0 @@
-module Rendering::ContentHelper
-end
