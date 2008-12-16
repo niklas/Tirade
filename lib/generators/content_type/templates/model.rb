9858288447285f88222f1c7cfc3e88b1c77ae3a8
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2008-12-16 20:23:11 +0000

    content_type generator defines Model.sample
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1712 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- lib/generators/content_type/templates/model.rb
+++ lib/generators/content_type/templates/model.rb
@@ -1,5 +1,13 @@
 class <%= class_name%> < ActiveRecord::Base
   acts_as_content :liquid => [<%= attributes.collect {|a| ":#{a.name}"}. join(', ')%>]
   attr_accessible <%= attributes.collect {|a| ":#{a.name}"}. join(', ')%>
+
+  def self.sample
+    new(
+      <% attributes.each_with_index do |attribute, attribute_index| -%>
+        :<%= attribute.name %> => nil<%= attribute_index == attributes.length - 1 ? '' : ','%>
+      <% end -%>
+    )
+  end
 end
 
