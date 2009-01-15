015686b52c9b329ee60f58c5753573c592273727
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2009-01-15 23:46:19 +0000

    break MVC in specs, accordingly to ApplicationController
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1729 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,10 +41,34 @@ Spec::Runner.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.before(:each) do
+    # break MVC like in ApplicationController
+    [Part, Rendering, Page].each do |klass|
+      klass.active_controller = mock_controller
+    end
+  end
 end
 
 def simple_preview
   sp = parts(:simple_preview)
   #sp.stub!(:liquid).and_return(%Q[<h2>{{ simple_preview.title }}</h2>\n<p>{{ simple_preview.body }}</p>])
   return sp
+end
+
+def mock_controller
+  finder = mock(ActionView::TemplateFinder)
+  def finder.pick_template(ppath,ext)
+    path = File.join(RAILS_ROOT, 'spec', 'fixtures', 'views', "#{ppath}.#{ext}")
+    File.exists?(path) ? path : false
+  end
+  view = mock(ActionView::Base)
+  view.stub!(:finder).and_return(finder)
+
+  controller = mock(PublicController)
+  controller.stub!(:current_theme).and_return('cool')
+  controller.stub!(:view_paths).and_return([ File.join(RAILS_ROOT,'spec','fixtures','views') ])
+  controller.stub!(:master_helper_module).and_return(PublicController.new.master_helper_module)
+  # there is no such thing as PublicController#template
+  controller.instance_variable_set('@template', view)
+  controller
 end
