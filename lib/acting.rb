11e0693cfeb5e99d5cd91b7780eb071ec5efbcf0
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2009-01-19 22:49:22 +0000

    fixed the acting
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1785 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- lib/acting.rb
+++ lib/acting.rb
@@ -1,5 +1,8 @@
 module Acting
   def self.included(base)
+    base.class_inheritable_reader :acting_roles
+    base.class_inheritable_writer :acting_roles
+    base.acting_roles = []
     base.class_eval do
       include InstanceMethods
       extend ClassMethods
@@ -9,16 +12,17 @@ module Acting
     def acts_as?(role)
       self.class.acts_as?(role)
     end
+    def acting_roles
+      self.class.acting_roles
+    end
   end
   module ClassMethods
     def acts_as?(role)
       acting_roles.include?(role.to_sym)
     end
-    def acting_roles
-      @@acting_roles ||= []
-    end
     def acts_as!(role)
       acting_roles << role.to_sym
+      acting_roles.uniq!
     end
   end
 end
