9ac8c7ad4364d394c0e59082ddfea563817bf7d5
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2009-01-20 20:06:25 +0000

    auto-load all filters in app/filters
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1802 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- config/initializers/liquid_filters.rb
+++ config/initializers/liquid_filters.rb
@@ -1,2 +1,12 @@
 # register all used filters
-Liquid::Template.register_filter(ImageFilter)
+Dir.glob(File.join(RAILS_ROOT,'app','filters','*.rb')).map do |path|
+  File.basename path
+end.map do |filename|
+  filename.gsub(/\.rb$/,'')
+end.map(&:classify).map(&:constantize).each do |mod|
+  begin
+    Liquid::Template.register_filter(mod)
+  rescue Liquid::ArgumentError => e
+    logger.info(e.message)
+  end
+end
