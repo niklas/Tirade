ba4bf86fa2f9e2559c76dbe421221c83d7df8049
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2009-02-05 18:12:36 +0000

    correct Page#generated_url after saving from form and root?==true
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1864 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/models/page.rb
+++ app/models/page.rb
@@ -81,7 +81,7 @@ class Page < ActiveRecord::Base
   def generated_url
     if !parent_id.nil?
       [parent.url, self.title.urlize].compact.join('/').sub(%r(^/),'')
-    elsif !@wanted_parent_id.nil?
+    elsif @wanted_parent_id.to_i != 0
       [Page.find_by_id(@wanted_parent_id).andand.url, title.urlize].compact.join('/').sub(%r(^/),'')
     else
       ''
