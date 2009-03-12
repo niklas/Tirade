ceb3702e4f45208dcb9fe535bb57967abefe794e
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2009-03-12 20:31:50 +0000

    price filter
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1876 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- app/filters/text_filter.rb
+++ app/filters/text_filter.rb
@@ -6,6 +6,14 @@ module TextFilter
       gsub(/[AEIOU]/, vowel.upcase)
   end
 
+  def price(val)
+    if val.to_f == 0.0
+      'free'
+    else
+      "&euro; #{sprintf('%.2f', val)}"
+    end
+  end
+
   def markup(text)
     RedCloth.new(text).to_html
   end
