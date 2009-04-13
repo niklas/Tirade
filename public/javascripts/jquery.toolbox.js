413f73d54b9289333792075b23c26be815fd9712
niklas <niklas@9df96bbd-a318-0410-8f51-977bef1cc4bb>
2009-04-13 00:54:57 +0000

    refresh page button, raw version
    git-svn-id: svn+ssh://lanpartei.de/home/svn/tirade/branches/tirade-v2@1921 9df96bbd-a318-0410-8f51-977bef1cc4bb

--- public/javascripts/jquery.toolbox.js
+++ public/javascripts/jquery.toolbox.js
@@ -48,6 +48,9 @@ var Toolbox = {
 
 
     this.sidebar(' ul.actions a.toggle.edit.grid').toggleEditPage();
+    this.sidebar(' ul.actions a.refresh_page').click(function(e) {
+      console.debug("Should refresh page");
+    });
 
     // Set Title of last frame
     this.frames(':not(:first)').livequery(function() { 
@@ -583,7 +586,8 @@ Toolbox.Templates = {
         { tagName: 'ul', class: 'history' },
         { tagName: 'ul', class: 'clipboard list records' },
         { tagName: 'ul', class: 'actions', childNodes: [
-          { tagName: 'li', childNodes: [  { tagName: 'a', class: 'toggle edit grid', childNodes: [ { tagName: 'img', src: '/images/icons/small/bulb.gif' }]}  ] }
+          { tagName: 'li', childNodes: [  { tagName: 'a', class: 'toggle edit grid', childNodes: [ { tagName: 'img', src: '/images/icons/small/bulb.gif' }]}  ] },
+          { tagName: 'li', childNodes: [  { tagName: 'a', class: 'refresh_page', childNodes: [ { tagName: 'img', src: '/images/icons/small/refresh.gif' }]}  ] }
         ] }
       ] },
       { tagName: 'div', class: 'body', childNodes: [
