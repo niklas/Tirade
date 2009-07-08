371a65ebc56617ac0cf79fec37917fcb1370e8ae
Niklas Hofer <niklas+dev@lanpartei.de>
2009-07-08 19:26:00 +0200

    corrected spec for NormalFormBuilder

--- spec/helpers/normal_form_builder_spec.rb
+++ spec/helpers/normal_form_builder_spec.rb
@@ -9,21 +9,20 @@ describe 'NormalFormBuilder' do
   end
 end
 
-describe 'NormalFormBuilder', 'in a form' do
+describe 'NormalFormBuilder', 'in a form with new Content' do
   before(:each) do
     ActionView::Base.send :include, ApplicationHelper
     ActionView::Base.send :include, InterfaceHelper # FIXME shouldn't Desert have done this already?
     @view = ActionView::Base.new
     @view.stub!(:url_for).and_return('/foo')
     @view.stub!(:protect_against_forgery?).and_return(false)
+    @content = Factory.build :content
+    @builder = NormalFormBuilder.new :content, Content.new, @view, {}, nil
   end
 
   describe "with a text_field" do
     before(:each) do
-      @html = _erbout = ''
-      @view.form_for(:content, Content.new, :url => '/foo', :builder => NormalFormBuilder) do |f|
-          _erbout << f.text_field(:title, :label => 'The slanky Title')
-      end
+      @html = @builder.text_field(:title, :label => 'The slanky Title')
     end
     it "should render something" do
       @html.should_not be_empty
@@ -38,10 +37,7 @@ describe 'NormalFormBuilder', 'in a form' do
 
   describe "with a text_area" do
     before(:each) do
-      @html = _erbout = ''
-      @view.form_for(:content, Content.new, :url => '/foo', :builder => NormalFormBuilder) do |f|
-          _erbout << f.text_area(:body, :label => 'The skinny Body')
-      end
+      @html = @builder.text_area(:body, :label => 'The skinny Body')
     end
     it "should have a DIV and the textarea with a label" do
       @html.should have_tag('div') do
@@ -53,11 +49,8 @@ describe 'NormalFormBuilder', 'in a form' do
 
   describe "with a select" do
     before(:each) do
-      @html = _erbout = ''
-      select_options = {:foo => 1, :bar => 2, :baz => 3}
-      @view.form_for(:content, Content.new, :url => '/foo', :builder => NormalFormBuilder) do |f|
-          _erbout << f.select(:state, select_options, { :label => 'Select the type' })
-      end
+      select_options = [["foo",1], ["bar", 2], ["baz", 3]]
+      @html = @builder.select(:state, select_options, { :label => 'Select the type' })
     end
     it "should have a DIV and the select field with options and a label" do
       @html.should have_tag('div') do
@@ -73,11 +66,8 @@ describe 'NormalFormBuilder', 'in a form' do
 
   describe "with a collection_select" do
     before(:each) do
-      @html = _erbout = ''
       select_options = [["foo",1], ["bar", 2], ["baz", 3]]
-      @view.form_for(:content, Content.new, :url => '/foo', :builder => NormalFormBuilder) do |f|
-          _erbout << f.collection_select(:state, select_options, :last, :first, { :label => 'Select the type' })
-      end
+      @html = @builder.collection_select(:state, select_options, :last, :first, { :label => 'Select the type' })
     end
     it "should have a DIV and the select field with options and a label" do
       @html.should have_tag('div') do
@@ -92,10 +82,7 @@ describe 'NormalFormBuilder', 'in a form' do
   end
   describe "with a check_box" do
     before(:each) do
-      @html = _erbout = ''
-      @view.form_for(:content, Content.new, :url => '/foo', :builder => NormalFormBuilder) do |f|
-          _erbout << f.check_box(:title, :label => 'A Checkbox for a String??')
-      end
+      @html = @builder.check_box(:title, :label => 'A Checkbox for a String??')
     end
     it "should render something" do
       @html.should_not be_empty
@@ -110,10 +97,7 @@ describe 'NormalFormBuilder', 'in a form' do
 
   describe "with a select_picture" do
     before(:each) do
-      @html = _erbout = ''
-      @view.form_for(:content, Content.new, :url => '/foo', :builder => NormalFormBuilder) do |f|
-          _erbout << f.select_picture
-      end
+      @html = @builder.select_picture
     end
     it "should render something" do
       @html.should_not be_empty
