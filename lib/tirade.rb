3b4cb84a0c3543d1486b4d9e77d1b60e34842bcb
Niklas Hofer <niklas+dev@lanpartei.de>
2009-07-16 15:56:14 +0200

    auto-define controllers for content types if they are missing

--- lib/tirade.rb
+++ lib/tirade.rb
@@ -3,5 +3,6 @@ require 'tirade/form_builder_helper'
 require 'tirade/normal_form_builder'
 require 'tirade/toolbox_form_builder'
 require 'tirade/active_record'
+require 'tirade/dependencies'
 
 Tirade::ActiveRecord.enable
---
+++ lib/tirade/dependencies.rb
@@ -0,0 +1,23 @@
+dependencies = ActiveSupport.const_defined?(:Dependencies) ? ActiveSupport::Dependencies : Dependencies
+dependencies.module_eval do
+  def load_missing_constant_with_tirade(from_mod, const_name)
+    from_mod = guard_against_anonymous_module(from_mod)
+    qualified_name = qualified_name_for from_mod, const_name
+    if qualified_name =~ /^(\w+)Controller$/i
+      class_name = $1.singularize
+      if Tirade::ActiveRecord::Content.class_names.include?(class_name)
+        ActiveRecord::Base::logger.debug("Tirade will define a #{qualified_name}")
+        Object.class_eval <<-EODECL
+          class #{qualified_name} < ApplicationController
+            feeds_toolbox_with :#{class_name.underscore}
+          end
+        EODECL
+        autoloaded_constants << qualified_name
+        return qualified_name.constantize
+      end
+    end
+    load_missing_constant_without_tirade(from_mod, const_name)
+  end
+
+  alias_method_chain :load_missing_constant, :tirade
+end
