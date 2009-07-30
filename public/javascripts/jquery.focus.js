ae2f2aa0238c03be2e5f9f47e16dc61b981e18c5
Niklas Hofer <niklas+dev@lanpartei.de>
2009-07-30 18:34:14 +0200

    rewrote focussable completly as jquery plugin

--- public/javascripts/jquery.focus.js
+++ public/javascripts/jquery.focus.js
@@ -1,202 +1,224 @@
-var Focus = {
-  on: function(element, options) {
-    var e = $(element).closest('.ui-focusable');
-    if (e.length == 0) return;
-    if (!this.exists()) this._createFrame();
-    this.to(e);
-    return this;
-  },
-  start: function() {
-    $('div.page:first').trigger('tirade.focus');
-  },
-  to: function(element) {
-    if (!this.exists()) return;
-    this.element = $(element);
-    var options = this.element.data('focus-options');
-    this.tabsToTop( options.children ?  $(options.children, this.element) : []);
-    this.tabsToLeft( options.left_children ?  $(options.left_children, this.element) : []);
-    this.setTitle( this.element.attr('title') || this.element.typeAndId().type );
-    this.showButton.attr('href', this.element.showUrl()  );
-    // this.clearButtons();
-    // this.setButtons(this.element.);
-    this.updateFrame();
-    return this;
-  },
-  stop: function() {
-    $(window).unbind('resize', Focus.sync);
-    $('body').animate({paddingTop: 0});
-    return this.frames().remove();
-  },
-  sync: function() {
-    Focus.updateFrame();
-  },
-  updateFrame: function() {
-    if (this.element.length == 0) return;
-    var element = this.element;
-    var border = 5;
-    var padding = 3;
-    var left = element.offset().left;
-    var top = element.offset().top;
-    var header = this.frameTop.outerHeight();
-    var eWidth = element.outerWidth({margin: true});
-    var dialogPadding = this.frameTop.outerWidth() - this.frameTop.width();
-
-    this.frameTop.show().animate({
-      top: top - header - padding, 
-      left: left - border - padding, 
-      width: eWidth + 2*border + 2*padding - dialogPadding
-    });
-    this.frameBottom.show().animate({
-      top: top + element.height() + padding, 
-      left: left - border - padding, 
-      height: border,
-      width: eWidth + 2*border + 2*padding
-    });
-    this.frameLeft.show().animate({
-      top: top - header - padding, 
-      left: left - border - padding, 
-      width: border,
-      height: element.height() + header + border + 2*padding
-    });
-    this.frameRight.show().animate({
-      top: top - header - padding, 
-      left: left + eWidth + padding, 
-      width: border,
-      height: element.height() + header + border + 2*padding
-    });
-  },
-  _createFrame: function() {
-    var classes = 'focus ui-widget';
-    this.frameBottom = $('<div/>')
-      .addClass('bottom ui-corner-bottom')
-      .addClass(classes)
-      .attr('role', 'focus')
-      .hide()
-      .appendTo($('body'));
-
-    this.frameLeft = $('<div/>')
-      .addClass('left ui-corner-left')
-      .addClass(classes)
-      .attr('role', 'focus')
-      .hide()
-      .appendTo($('body'));
-
-    this.frameRight = $('<div/>')
-      .addClass('right ui-corner-right')
-      .addClass(classes)
-      .attr('role', 'focus')
-      .hide()
-      .appendTo($('body'));
-
-    this.frameTop = $('<div/>')
-      .addClass('top ui-dialog ui-widget-content ui-corner-top')
-      .addClass(classes)
-      .attr('role', 'focus')
-      .hide()
-      .appendTo($('body'));
-
-    this.titleBarTop = $('<div/>')
-      .addClass('ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix')
-      .attr('role', 'titlebar')
-      .appendTo(this.frameTop);
-
-    this.backButton = $.ui.button({icon: 'circle-triangle-w', text: 'back', class: 'back'})
-      .click( this.back )
-      .appendTo(this.titleBarTop);
-
-    this.title = $('<span/>')
-      .addClass('ui-dialog-title title')
-      .text('Title')
-      .appendTo(this.titleBarTop);
-
-    var tabs = $('<div />').addClass('ui-tabs').hide();
-
-    this.tabsTop = $('<ul />')
-      .addClass('ui-tabs-nav ui-widget-header ui-helper-reset ui-corner-all ui-helper-clearfix')
-      .appendTo( tabs.clone().appendTo(this.frameTop) );
-
-    this.tabsLeft = $('<ul />')
-      .addClass('ui-tabs-nav ui-tabs ui-widget-header ui-helper-reset ui-corner-all ui-helper-clearfix')
-      .appendTo( tabs.clone().appendTo(this.frameLeft) );
-
-    this.showButton = $.ui.button({icon: 'circle-triangle-e', text: 'show', class: 'show with_toolbox'})
-      .appendTo(this.titleBarTop);
-
-    $(window).bind('resize', Focus.sync);
-    $('body').css('padding-top', '5em');
-  },
-  exists: function() {
-    return this.frames().length > 0
-  },
-  frames: function() {
-    return $('body div.focus')
-  },
-  setTitle: function(text, place) {
-    return this.title.text(text);
-  },
-  back: function() {
-    if (!Focus.exists()) return;
-    var options = Focus.element.data('focus-options');
-    if (options.parent) {
-      var parent = Focus.element.parent().closest(options.parent);
-      if (parent.length == 1) {
-        parent.trigger('tirade.focus');
-      }
-    }
-  },
-  tabsToTop: function(elements) {
-    this.tabsTo( Focus.tabsTop, elements );
-  },
-  tabsToLeft: function(elements) {
-    this.tabsTo( Focus.tabsLeft, elements );
-  },
-  tabsTo: function(container, elements) {
-    if (elements.length == 0) {
-      container.parent().hide();
-      return;
-    }
-    container
-      .find('li').remove().end()
-      .parent().show();
-    return elements.each( function() {
-      var $self = $(this);
-      var link = $('<a />')
-        .attr('href', '#')
-        .text( $(this).attr('title') || $(this).typeAndId().type )
-        .click(function() { $self.trigger('tirade.focus') })
-        .wrap('<li></li>')
-        .parent()
-        .addClass('ui-state-default')
-        .appendTo( container );
-      if (container.closest('.focus').is('.left'))
-        link.height($self.height());
-      else
-        link.width($self.width());
-    });
-  }
-};
-
-
-$.fn.focusable = function(options) {
-  var defaults = {
-    parent: 'div',
-    children: 'div',
-    side_children: null
+(function($) {
+  $.focusable = function(action, new_options) {
+    // events.trigger('event');
+    return options;
   };
-  var options = $.extend(defaults, options);
 
-  return $(this)
-    .data('focus-options', options)
-    .addClass('ui-focusable')
-    .bind('tirade.focus', function(event) {
-      Focus.on(event.target);
-      event.stopPropagation();
-      return false;
-    });
-};
+  $.extend( $.focusable, {
+    defaults: {
+      parent: 'div',
+      children: 'div',
+      side_children: null
+    },
+    frameClasses: 'focus ui-widget',
+    current: null,
+    exists: function() {
+      return $.focusable.frames().length > 0;
+    },
+    open: function() {
+      var classes = $.focusable.frameClasses;
+      $.focusable.frameBottom = $('<div/>')
+        .addClass('bottom ui-corner-bottom')
+        .addClass(classes)
+        .attr('role', 'focus')
+        .hide()
+        .appendTo($('body'));
 
-$( function() {
-  $('div.page').focusable({parent: null, children: '>div.grid'});
-  $('div.grid').focusable({parent: 'div.grid, div.page', children: '> * > div.grid,>div.grid', left_children: '>div.rendering'});
-  $('div.rendering').focusable({parent: 'div.grid', children: null});
-});
+      $.focusable.frameLeft = $('<div/>')
+        .addClass('left ui-corner-left')
+        .addClass(classes)
+        .attr('role', 'focus')
+        .hide()
+        .appendTo($('body'));
+
+      $.focusable.frameRight = $('<div/>')
+        .addClass('right ui-corner-right')
+        .addClass(classes)
+        .attr('role', 'focus')
+        .hide()
+        .appendTo($('body'));
+
+      $.focusable.frameTop = $('<div/>')
+        .addClass('top ui-dialog ui-widget-content ui-corner-top')
+        .addClass(classes)
+        .attr('role', 'focus')
+        .hide()
+        .appendTo($('body'));
+
+      $.focusable.titleBarTop = $('<div/>')
+        .addClass('ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix')
+        .attr('role', 'titlebar')
+        .appendTo($.focusable.frameTop);
+
+      $.focusable.backButton = $.ui.button({icon: 'circle-triangle-w', text: 'back', class: 'back'})
+        .click( $.focusable.back )
+        .appendTo($.focusable.titleBarTop);
+
+      $.focusable.titleElement = $('<span/>')
+        .addClass('ui-dialog-title title')
+        .text('Title')
+        .appendTo($.focusable.titleBarTop);
+
+      var tabs = $('<div />').addClass('ui-tabs').hide();
+
+      $.focusable.tabsTop = $('<ul />')
+        .addClass('ui-tabs-nav ui-widget-header ui-helper-reset ui-corner-all ui-helper-clearfix')
+        .appendTo( tabs.clone().appendTo($.focusable.frameTop) );
+
+      $.focusable.tabsLeft = $('<ul />')
+        .addClass('ui-tabs-nav ui-tabs ui-widget-header ui-helper-reset ui-corner-all ui-helper-clearfix')
+        .appendTo( tabs.clone().appendTo($.focusable.frameLeft) );
+
+      $.focusable.showButton = $.ui.button({icon: 'circle-triangle-e', text: 'show', class: 'show with_toolbox'})
+        .appendTo($.focusable.titleBarTop);
+
+      $(window).bind('resize', $.focusable.sync);
+      $('body').css('padding-top', '5em');
+    },
+    start: function() {
+      $.focusable.on( $('.ui-focusable:first') );
+    },
+    stop: function() {
+      $(window).unbind('resize', $.focusable.sync);
+      $('body').animate({paddingTop: 0});
+      return $.focusable.frames().remove();
+    },
+    on: function(element) {
+      var e = $(element).closest('.ui-focusable');
+      if (e.length == 0) return;
+      if (!$.focusable.exists()) $.focusable.open();
+      $.focusable.goto(e);
+      return this;
+    },
+    goto: function(element) {
+      var e =  $(element).closest('.ui-focusable');
+      if (e.length == 0) return;
+      $.focusable.current = e;
+      var options = e.data('focusable:options');
+      $.focusable.fillTopTabs( options.children ?  $(options.children, e) : []);
+      $.focusable.fillLeftTabs( options.left_children ?  $(options.left_children, e) : []);
+      $.focusable.title( e.attr('title') || e.typeAndId().type );
+      $.focusable.showButton.attr('href', e.showUrl()  );
+      // $.focusable.clearButtons();
+      // $.focusable.setButtons(this.element.);
+      $.focusable.sync();
+    },
+    back: function() {
+      if (!$.focusable.exists()) return;
+      var e = $.focusable.current;
+      var options = e.data('focusable:options');
+      if (options.parent) {
+        var parent = e.parent().closest(options.parent).closest('.ui-focusable');
+        if (parent.length == 1) {
+          parent.trigger('tirade.focus');
+        }
+      }
+    },
+    sync: function() {
+      var e = $.focusable.current;
+      if (e.length == 0) return;
+
+      var border = 5;
+      var padding = 3;
+      var left = e.offset().left;
+      var top = e.offset().top;
+      var header = $.focusable.frameTop.outerHeight();
+      var eWidth = e.outerWidth({margin: true});
+      var dialogPadding = $.focusable.frameTop.outerWidth() - $.focusable.frameTop.width();
+
+      $.focusable.frameTop.show().animate({
+        top: top - header - padding, 
+        left: left - border - padding, 
+        width: eWidth + 2*border + 2*padding - dialogPadding
+      });
+      $.focusable.frameBottom.show().animate({
+        top: top + e.height() + padding, 
+        left: left - border - padding, 
+        height: border,
+        width: eWidth + 2*border + 2*padding
+      });
+      $.focusable.frameLeft.show().animate({
+        top: top - header - padding, 
+        left: left - border - padding, 
+        width: border,
+        height: e.height() + header + border + 2*padding
+      });
+      $.focusable.frameRight.show().animate({
+        top: top - header - padding, 
+        left: left + eWidth + padding, 
+        width: border,
+        height: e.height() + header + border + 2*padding
+      });
+    },
+    title: function(new_title) {
+      $.focusable.titleElement.text(new_title);
+    },
+    frames: function() {
+      return $('body div.focus')
+    },
+    fillTopTabs: function(elements) { 
+      return $.focusable.fillTabs( $.focusable.tabsTop, elements );
+    },
+    fillLeftTabs: function(elements) { 
+      return $.focusable.fillTabs( $.focusable.tabsLeft, elements );
+    },
+    fillTabs: function(container, elements) {
+      if (!container) return;
+      elements = $(elements).filter('.ui-focusable');
+      if (elements.length == 0) {
+        container.parent().hide();
+        return;
+      }
+      container
+        .find('li').remove().end()
+        .parent().show();
+      return elements.each( function() {
+        var $self = $(this);
+        var link = $('<a />')
+          .attr('href', '#')
+          .text( $self.attr('title') || $self.typeAndId().type )
+          .click(function() { $.focusable.goto($self) })
+          .wrap('<li></li>')
+          .parent()
+          .addClass('ui-state-default')
+          .appendTo( container );
+        if (container.closest('.focus').is('.left'))
+          link.height($self.height());
+        else
+          link.width($self.width());
+      });
+    }
+  });
+
+  $.fn.focusable = function(action, options) {
+    if (typeof(action) == 'object') {
+      options = action;
+    };
+    return this
+      .each(function() {
+        e = $(this);
+        //$.each(options, function(event, fn) {
+        //  if (typeof(fn) == 'function')
+        //    e.bind(event, fn);
+        //});
+        //e.trigger('event');
+        options = $.extend({}, $.focusable.defaults, e.data('focusable:options'), options);
+        e.data('focusable:options', options);
+      })
+      .addClass('ui-focusable')
+      .bind('tirade.focus', function(event) {
+        $.focusable.on(event.target);
+        event.stopPropagation();
+        return false;
+      });
+  };
+
+  $( function() {
+    $('div.page').focusable({parent: null, children: '>div.grid'});
+    $('div.grid').focusable({parent: 'div.grid, div.page', children: '> * > div.grid,>div.grid', left_children: '>div.rendering'});
+    $('div.rendering').focusable({parent: 'div.grid', children: null});
+  });
+
+})(jQuery);
+
+
