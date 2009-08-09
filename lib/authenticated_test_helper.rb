fb922c52ae782b887c7a4a32bdccc6f8653c6ec0
Niklas Hofer <niklas+dev@lanpartei.de>
2009-08-09 17:46:02 +0200

    AuthenticatedTestHelper supports authlogic

--- lib/authenticated_test_helper.rb
+++ lib/authenticated_test_helper.rb
@@ -1,10 +1,27 @@
 module AuthenticatedTestHelper
   # Sets the current user in the session from the user fixtures.
   def login_as(user)
-    @request.session[:user_id] = user ? users(user).id : nil
+    Authlogic::Session::Base.controller = self
+    UserSession.create user ? users(user).id : nil
   end
 
   def authorize_as(user)
     @request.env["HTTP_AUTHORIZATION"] = user ? ActionController::HttpAuthentication::Basic.encode_credentials(users(user).login, 'test') : nil
   end
+
+  #def current_user(stubs = {})
+  #  @current_user ||= mock_model(User, stubs)
+  #end
+  # 
+  #def user_session(stubs = {}, user_stubs = {})
+  #  @current_user ||= mock_model(UserSession, {:user => current_user(user_stubs)}.merge(stubs))
+  #end
+  # 
+  #def login(session_stubs = {}, user_stubs = {})
+  #  UserSession.stub!(:find).and_return(user_session(session_stubs, user_stubs))
+  #end
+  # 
+  #def logout
+  #  @user_session = nil
+  #end
 end
