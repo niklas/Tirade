d63644b17b9926d9bcdfc711240f37410fe235b4
Niklas Hofer <niklas+dev@lanpartei.de>
2009-08-12 22:26:19 +0200

    Toolbox- and InterfaceHelper systemwide

--- vendor/plugins/toolbox/init.rb
+++ vendor/plugins/toolbox/init.rb
@@ -4,5 +4,9 @@ require 'toolbox/model'
 require 'toolbox/view'
 
 ActionController::Base.class_eval { include Tirade::Toolbox::Controller }
+ApplicationController.class_eval do
+  helper :toolbox
+  helper :interface
+end
 ActiveRecord::Base.class_eval { include Tirade::Toolbox::Model }
 ActionView::Base.class_eval { include Tirade::Toolbox::View }
