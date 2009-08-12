06493d8ee8ed7b0ef2b105a47195cb056c9508a5
Niklas Hofer <niklas+dev@lanpartei.de>
2009-08-12 14:17:37 +0200

    preload content types from config/application.yml

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,7 +1,13 @@
 begin
-  APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/application.yml")[RAILS_ENV]
+  APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/application.yml")[RAILS_ENV].with_indifferent_access
 rescue
-  puts "could not find config/application.yml - some features/setting may be missing."
+  STDERR.puts "could not find config/application.yml - some features/setting may be missing."
 end
 
 SITE_URL = APP_CONFIG[:url] || 'http://localhost:3000'
+
+# This preloads the content types so that they are registered as Tirade::ActiveRecord::Content
+
+APP_CONFIG[:content_types].each do |typ|
+  typ.to_s.constantize
+end
