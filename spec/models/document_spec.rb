4c4d2f0d00f6e101a6acb73771f94dc600c8f811
Niklas Hofer <niklas+dev@lanpartei.de>
2009-08-14 18:55:55 +0200

    unneccessary specs for Document#title.locale

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -8,4 +8,40 @@ describe Document do
   it "should act as slugged" do
     Document.should be_acts_as(:slugged)
   end
+
+  describe "from Factory" do
+    before( :each ) do
+      @document = Factory(:document)
+    end
+
+    it "should be a Document" do
+      @document.should be_a(Document)
+    end
+
+    it "should be valid" do
+      @document.should be_valid
+    end
+
+    it "should have a title" do
+      @document.title.should_not be_blank
+    end
+
+
+    describe "in locale 'de'" do
+      before( :each ) do
+        I18n.locale = :de
+      end
+
+      it "should fall back on en locale" do
+        @document.title.should be_fallback
+        @document.title.locale.should == :en
+        @document.title.requested_locale.should == :de
+      end
+
+      it "should still have a title by falling back" do
+        @document.title.should_not be_blank
+      end
+    end
+
+  end
 end
