6673ab4475b9a4ac685028c432fc28412806aab7
Niklas Hofer <niklas+dev@lanpartei.de>
2009-08-17 13:31:39 +0200

    can switch language from dashboard

--- config/initializers/globalize.rb
+++ config/initializers/globalize.rb
@@ -1,6 +1,6 @@
 I18n.default_locale = :en
 
+I18n.load_path += Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')]
 require 'globalize/i18n/missing_translations_log_handler'
 I18n.exception_handler = :missing_translations_log_handler
-logger = Logger.new("#{RAILS_ROOT}/log/missing_translations.log")
-I18n.missing_translations_logger = logger
+I18n.missing_translations_logger = Logger.new("#{RAILS_ROOT}/log/missing_translations.log")
