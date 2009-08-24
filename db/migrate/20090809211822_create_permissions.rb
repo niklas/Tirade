8e9cdc2879740f1d3948e758f0b158bb1c5348cb
Niklas Hofer <niklas+dev@lanpartei.de>
2009-08-24 15:54:54 +0200

    must create permissions table

--- db/migrate/20090809211822_create_permissions.rb
+++ db/migrate/20090809211822_create_permissions.rb
@@ -1,21 +1,19 @@
 class CreatePermissions < ActiveRecord::Migration
   def self.up
-    remove_column :permissions, :app_controller
-    remove_column :permissions, :app_method
-    remove_column :permissions, :processed
-    add_column :permissions, :name, :string
+    create_table :permissions do |t|
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.string   "name"
+    end
 
-		create_table :permissions_user_groups, :id => false do |t|
+    create_table :permissions_user_groups, :id => false do |t|
       t.integer :permission_id
       t.integer :user_group_id
     end
   end
 
   def self.down
-		drop_table :permissions_user_groups
-    remove_column :permissions, :name
-    add_column :permissions, "app_controller", :string  
-    add_column :permissions, "app_method"    , :string  
-    add_column :permissions, "processed"     , :boolean 
+    drop_table :permissions_user_groups
+    drop_table :permissions
   end
 end
