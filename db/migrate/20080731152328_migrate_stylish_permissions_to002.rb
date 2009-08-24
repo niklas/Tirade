3d5ba2fafb6c014abc7a2e31761997814a281df5
Niklas Hofer <niklas+dev@lanpartei.de>
2009-08-24 15:50:45 +0200

    removed desert-based migrations

--- db/migrate/20080731152328_migrate_stylish_permissions_to002.rb
+++
@@ -1,9 +0,0 @@
-class MigrateStylishPermissionsTo002 < ActiveRecord::Migration
-  def self.up
-    migrate_plugin 'stylish_permissions', 2
-  end
-
-  def self.down
-    migrate_plugin 'stylish_permissions', 1
-  end
-end
--- db/migrate/20080814130604_migrate_rampant_to002.rb
+++
@@ -1,9 +0,0 @@
-class MigrateRampantTo002 < ActiveRecord::Migration
-  def self.up
-    migrate_plugin 'rampant', 2
-  end
-
-  def self.down
-    migrate_plugin 'rampant', 0
-  end
-end
