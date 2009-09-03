5c5a120e43252906e675f873cc745b98e2752f00
Niklas Hofer <niklas+dev@lanpartei.de>
2009-09-03 15:09:48 +0200

    LazyNestedSet does not give wanted_parent_id if non wanted

--- lib/lazy_nested_set.rb
+++ lib/lazy_nested_set.rb
@@ -1,18 +1,20 @@
 # adds some methods to Models to set a #wanted_parent_id that gets applied after saving
 module LazyNestedSet
   def wanted_parent_id
-     @wanted_parent_id || self.read_attribute(:parent_id)
+     @wanted_parent_id
   end
 
   def wanted_parent_id=(new_parent_id)
-    @wanted_parent_id = new_parent_id.to_i
+    @wanted_parent_id = new_parent_id
   end
 
   def move_to_parent_if_wanted
-    if !@wanted_parent_id.nil? && (new_parent = self.class.table_name.classify.constantize.find_by_id(@wanted_parent_id))
-      transaction do
-        self.move_to_child_of new_parent
-        self.after_move if self.respond_to?(:after_move)
+    if !wanted_parent_id.blank? && (new_parent = self.class.table_name.classify.constantize.find_by_id(wanted_parent_id))
+      if new_parent.id != self.parent_id
+        transaction do
+          self.move_to_child_of new_parent
+          self.after_move if self.respond_to?(:after_move)
+        end
       end
     end
     @wanted_parent_id = nil
