5a69de03cc62943f3fb6b9a25d072be58743def1
Niklas Hofer <niklas+dev@lanpartei.de>
2009-09-18 01:28:23 +0200

    removed old stuff

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,25 +2,6 @@ class ImagesController < ManageResourceController::Base
   in_place_edit_for :image, :title
   skip_before_filter :login_required, :only => :custom
 
-  # TODO append .js in multipartform
-  # TODO multiple jupload with flash or so..
-  def old_create
-    @model = @image = Image.new(params[:image])
-
-    if @image.save
-      #@image.image_content_type = MIME::Types.type_for(@image.original_filename).to_s 
-      @models = @images = [@image] + @image.multiple_images
-      @model = @image = @images.first
-      responds_to_parent do
-        render_toolbox_action :created
-      end
-    else
-      responds_to_parent do
-        render_toolbox_action :failed_create
-      end
-    end
-  end
-
   def custom
     @image = Image.find(params[:id])
     @thumb = @image.scale_to(params[:geometry])
