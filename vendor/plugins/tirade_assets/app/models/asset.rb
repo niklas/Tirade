61f9109b3bec800a25b363ceeb384f2b4cad0d45
Niklas Hofer <niklas+dev@lanpartei.de>
2009-09-28 15:35:46 +0200

    Asset#file_url in liquid

--- vendor/plugins/tirade_assets/app/models/asset.rb
+++ vendor/plugins/tirade_assets/app/models/asset.rb
@@ -1,5 +1,5 @@
 class Asset < ActiveRecord::Base
-  acts_as_content :liquid => [:title, :description]
+  acts_as_content :liquid => [:title, :description, :file_url]
   attr_accessible :title, :description, :file
   markup :description
 
@@ -7,6 +7,10 @@ class Asset < ActiveRecord::Base
     :path => ":rails_root/public/upload/:attachment/:id/:style/:basename.:extension",
     :url => "/upload/:attachment/:id/:style/:basename.:extension"
   validates_attachment_presence :file
+
+  def file_url
+    file.url
+  end
 
   def self.sample
     new(
