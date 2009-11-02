5053ed941d7675c4f7824272acf8a8938f170357
Niklas Hofer <niklas+dev@lanpartei.de>
2009-11-02 22:39:50 +0100

    check @current_user(_session) for #present?

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -10,12 +10,12 @@ module AuthenticatedSystem
 
   protected
     def current_user_session
-      return @current_user_session if defined?(@current_user_session)
+      return @current_user_session if defined?(@current_user_session) && @current_user_session.present?
       @current_user_session = UserSession.find
     end
 
     def current_user
-      return @current_user if defined?(@current_user)
+      return @current_user if defined?(@current_user) && @current_user.present?
       if @current_user = current_user_session && current_user_session.user
         add_lockdown_session_values(@current_user)
         @current_user
