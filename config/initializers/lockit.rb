00b1444570969903babdf238161683cfd343062f
Niklas Hofer <niklas+dev@lanpartei.de>
2009-11-02 22:40:13 +0100

    remove method Lockdown::Session#logged_in?

--- config/initializers/lockit.rb
+++ config/initializers/lockit.rb
@@ -1,6 +1,7 @@
-require 'lockdown'
-
-# FIXME monkey patch until it can be overwritten cleanly
-module Lockdown::Frameworks::Rails::Controller::Lock
-  include AuthenticatedSystem
+# we bring our own logged_in? method in AuthenticatedSystem
+#
+# because Lockdown includes a Module with this method into AController::Base or
+# ApplicationController (dependant of the RAILS_ENV) we remove it completely. 
+Lockdown::Session.module_eval do
+  remove_method :logged_in?
 end
