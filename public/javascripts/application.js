fc8c34f5deebd1c43940c5bf267456a312bdb5cc
Niklas Hofer <niklas+dev@lanpartei.de>
2010-01-18 18:37:24 +0100

    fixed locale switcher

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,13 +6,13 @@
     var $locale_switcher = $('<ul class="locale_switcher" />');
     $('head link.alternate_locale').each(function (i, link) {
       var $link = $(link);
-      console.debug("link to ", $link.attr('href') );
       $('<a />')
         .attr('href', $link.attr('href'))
         .attr('title', $link.attr('lang'))
-        .addClass( window.location.pathname == $link.attr('href') ? 'active' : '' )
+        .addClass( window.location.pathname == $link.attr('href') ? 'active' : null )
         .text( $link.attr('lang') )
-        .appendTo( $locale_switcher );
+        .appendTo( $locale_switcher )
+        .wrap('<li></li>');
     });
     $locale_switcher.appendTo('body > div.page_margins > div.page:first');
 
